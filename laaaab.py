6f3bef39719da4d08b5356d67ba0c4ba150b9b2c
asmita016219 <as7625363@gmail.com>
2021-12-02 00:31:57 -0800

    first  project

---
+++ laaaab.py
@@ -0,0 +1,6 @@
+b=int(input("enter base of triangle"))
+h=int(input("enter height of triangle"))
+b=int(input("enter base of tringle"))
+h=int(input("enter height of triangle"))
+area=1/2*b*h
+print("the area of right angled triangle is{}cm\u00b2" .format(area))
